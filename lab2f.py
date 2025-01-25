feeaf6767834cbccd97e5af03e7999a36e8dd2f6
Tanuj Gupta <tgupta25@myseneca.ca>
2025-01-25 16:32:08 -0500

    Added 2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author : Tanuj Gupta
+# Author ID: 162349229
+# Date Created: 2025/01/25
+
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
