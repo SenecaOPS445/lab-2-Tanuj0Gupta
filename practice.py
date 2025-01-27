4584c204f57c595e63bf3a95e9963fa5d3a5afef
Tanuj Gupta <tgupta25@myseneca.ca>
2025-01-27 17:52:22 -0500

    Practice Week2

---
+++ practice.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# import sys
+
+# arguments = sys.argv
+# name = sys.argv[0]
+
+# print('Print out ALL script arguments: ', arguments)
+# print('Print out the script name: ' + name)
+# print('Print out the number of argument: ', len(sys.argv))
+
+# if True:
+#     print('This print is apart of the if statement')
+
+# if False:
+#     print('This first print statement will never run')
+#     print('This second print statement will also not run')
+# print('This print statement will run')
+
+# password = input('Please enter a secret password')
+# if password == 'P@ssw0rd':
+#     print('You have succesfully used the right password')
+
+# import sys
+
+# print(len(sys.argv))
+
+# if len(sys.argv) != 10:
+#     print('you do not have 10 arguments')
+#     sys.exit()
+
+a = 10
+b = 10
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('b is greater than a')
+else:
+    print('a is not greater than b')
+    print('a is not less than b')
+    print('Therefore, a is equal to b')
